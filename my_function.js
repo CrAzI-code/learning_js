2b32abe10c8737c549497a74d543467601c62238
CrAzI-code <emmaobiora96@gmail.com>
2022-05-28 19:28:08 -0700

    playing with javascript DOM

---
+++ my_function.js
@@ -0,0 +1,27 @@
+// let myPoints = 3
+
+// function add3Point(){
+//     myPoints += 3
+// }
+
+// function remove1Point(){
+//     myPoints -= 1
+// }
+
+// add3Point()
+// remove1Point()
+// add3Point()
+// remove1Point()
+// add3Point()
+
+// console.log(myPoints)
+
+let getErrorMsg = document.getElementById("error")
+console.log(getErrorMsg)
+
+
+function purchase(){
+    
+    getErrorMsg.textContent = "something went wrong, please try again"
+}
+
